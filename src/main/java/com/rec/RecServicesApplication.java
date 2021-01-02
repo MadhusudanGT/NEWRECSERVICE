7df704c6aef35f8576eb025311f53608e8c21e1d
shiv-hub <shivkumarmunda0097@gmail.com>
2021-01-02 12:47:25 +0530

    initial commit

---
+++ src/main/java/com/rec/RecServicesApplication.java
@@ -0,0 +1,13 @@
+package com.rec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecServicesApplication.class, args);
+	}
+
+}
