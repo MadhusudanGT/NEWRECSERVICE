6e88ff11ccff1076bf4b945a204ecba8358085ac
umesh <umeshkatakam7@gmail.com>
2021-01-04 12:28:01 +0530

    added seed data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,47 @@
+INSERT INTO rec."user"
+(adhar, created_at, dob, email, first_name, last_name, status, updated_at)
+VALUES(222222, CURRENT_TIMESTAMP, '29-01-1997', 'u@email.com', 'umesh', 'katakam', 'Active', CURRENT_TIMESTAMP);
+
+
+INSERT INTO rec."user"
+(adhar, created_at, dob, email, first_name, last_name, status, updated_at)
+VALUES(333333, CURRENT_TIMESTAMP, '30-01-1998', 'viswa@email.com', 'viswa', 'chand', 'Active', CURRENT_TIMESTAMP);
+
+INSERT INTO rec."user"
+(adhar, created_at, dob, email, first_name, last_name, status, updated_at)
+VALUES(444444, CURRENT_TIMESTAMP, '29-01-1999', 'shiv@email.com', 'shiv', 'munda', 'Active', CURRENT_TIMESTAMP);
+
+INSERT INTO rec."user"
+(adhar, created_at, dob, email, first_name, last_name, status, updated_at)
+VALUES(555555, CURRENT_TIMESTAMP, '29-01-1993', 'madhu@email.com', 'madhu', 'sudhan', 'Active', CURRENT_TIMESTAMP);
+
+INSERT INTO rec."user"
+(adhar, created_at, dob, email, first_name, last_name, status, updated_at)
+VALUES(666666, CURRENT_TIMESTAMP, '29-01-1999', 'swagata@email.com', 'swagata', 'madal', 'Active', CURRENT_TIMESTAMP);
+
+
+
+
+INSERT INTO rec.contact_model
+(address_line1, address_line2, country, phone_no1, phone_no2, pincode, state, status, user_id_id)
+VALUES('katamayyabazar', 'nakrekal', 'india', '9999999999', '9999', 508211, 'Ts', 'Selected', 1);
+
+INSERT INTO rec.contact_model
+(address_line1, address_line2, country, phone_no1, phone_no2, pincode, state, status, user_id_id)
+VALUES('kornul', 'kurnol', 'india', '999999999', '9999999999', 508222, 'AP', 'Selected', 2);
+
+INSERT INTO rec.contact_model
+(address_line1, address_line2, country, phone_no1, phone_no2, pincode, state, status, user_id_id)
+VALUES('maharasta', 'maharasta', 'india', '999999999', '9999990000', '505050','Mharastra', 'Selected',  3);
+
+INSERT INTO rec.contact_model
+(address_line1, address_line2, country, phone_no1, phone_no2, pincode, state, status, user_id_id)
+VALUES('Mumbai', 'mumbau', 'india', '8888888', '88888', 509211, 'Maharastra', 'Selected', 4);
+
+INSERT INTO rec.contact_model
+(address_line1, address_line2, country, phone_no1, phone_no2, pincode, state, status, user_id_id)
+VALUES('Bengalore', 'Bengalore', 'india', '44444444', '4444444', 564000, 'Karnataka', 'Selected', 5);
+
+
+
+
